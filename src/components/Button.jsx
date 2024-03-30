57704d67f819896e13e012b457cd36ec7c3da2e2
cinnamonjs <p.thunwattanakul@gmail.com>
2024-03-30 14:04:13 +0700

    hotfix: button merge class

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,18 @@
+import { cn } from "@/utils/cn"
+import { forwardRef } from "react";
+
 // re-use component with parameter input
 // children is inside component
 // ex. <Button> <p>abcd</p> <Button>; <p>abcd</p> is children of <Button>
-
-export const Button = ({ onClick, type, disabled, children }) => {
+export const Button = forwardRef(({ onClick, type, disabled, className, children }) => {
   return (
     <button
       type={type}
       disabled={disabled}
-      className="px-2 py-1 capitalize transition duration-150 border border-white rounded-lg hover:bg-white/15 active:scale-95"
+      className={cn("px-2 py-1 capitalize transition duration-150 border border-white rounded-lg hover:bg-white/15 active:scale-95", className)}
       onClick={onClick}
     >
       {children}
     </button>
   );
-};
+});
